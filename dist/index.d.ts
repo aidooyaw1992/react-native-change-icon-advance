84934d9885e670e78bbd97c90870be01a7736229
Daniel Aidoo <daniel.aidoo@mtn.com>
2025-03-18 11:45:13 +0000

    fix: update the resoluve function

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 export declare const changeIcon: (iconName?: string) => Promise<string>;
 export declare const resetIcon: () => Promise<string>;
 export declare const getIcon: () => Promise<string>;
+export declare const resolveAndroidEntryPoint: (activityToBeDisabled: string) => void;
 export declare const notifyExternalActivityStarting: () => Promise<void>;
 export declare const notifyExternalActivityFinished: () => Promise<void>;
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
-import { NativeModules } from "react-native";
+import { NativeModules, Platform } from "react-native";
 export const changeIcon = (iconName) => NativeModules.ChangeIcon.changeIcon(iconName);
 export const resetIcon = () => changeIcon();
 export const getIcon = () => NativeModules.ChangeIcon.getIcon();
-export const resolveAndroidEntryPoint = (activityToBeDisabled) => NativeModules.ChangeIcon.resolveEntryPoint(activityToBeDisabled);
+export const resolveAndroidEntryPoint = (activityToBeDisabled) => {
+    if (Platform.OS == 'android') {
+        NativeModules.ChangeIcon.resolveEntryPoint(activityToBeDisabled);
+    }
+};
 export const notifyExternalActivityStarting = () => NativeModules.ChangeIcon.notifyExternalActivityStarting();
 export const notifyExternalActivityFinished = () => NativeModules.ChangeIcon.notifyExternalActivityFinished();
